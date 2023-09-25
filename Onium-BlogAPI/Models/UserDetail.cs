532a97f102f690f785df6d105eae1d5d9b3b0123
LuisOnium <luis.rosa@onium.com.br>
2023-09-25 09:15:25 -0300

    Project criation

---
+++ Onium-BlogAPI/Models/UserDetail.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Onium_BlogAPI.Models
+{
+    public class UserDetail
+    {
+        [Key]
+        public int UserDetailId { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Password { get; set; }
+
+    }
+}
